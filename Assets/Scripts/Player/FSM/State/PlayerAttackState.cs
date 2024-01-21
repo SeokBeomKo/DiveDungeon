cac7da5a338094b1466b358996164218d24b8bed
mangruf <pinky0927@naver.com>
2024-01-22 01:18:01 +0900

    Update : 플레이어 FSM 공격 상태 수정 점프 중 공격 가능

--- Assets/Scripts/Player/FSM/State/PlayerAttackState.cs
+++ Assets/Scripts/Player/FSM/State/PlayerAttackState.cs
@@ -19,31 +19,47 @@ public class PlayerAttackState : IPlayerState
 
     public HashSet<PlayerMovementEnums> logicHash { get; } = new HashSet<PlayerMovementEnums>()
     {
-        PlayerMovementEnums.IDLE
+        PlayerMovementEnums.IDLE,
+        PlayerMovementEnums.JUMP,
+        PlayerMovementEnums.FALL
     };
 
     int curIndex;
 
     public void Update()
     {
+        // 공격 입력 값 delay
         if(player.animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.5f)
         {
             player.isAttack = false;
         }
 
-        if (player.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f)
+        if (player.animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.99f) return;
+
+        // 공격 애니메이션 순서대로 나오게
+        if (player.isAttack)
         {
-            if(player.isAttack)
-            {
-                player.animator.Play("Attack" + curIndex);
-                curIndex++;
-                if (curIndex > 3) curIndex = 1;
-            }
-            else
-            {
-                stateMachine.ChangeStateLogic(PlayerMovementEnums.IDLE);
-            }
+            InitializeAttackAnimation();
+            return;
         }
+        
+        // 플레이어가 땅에 있는지 체크
+        if (player.CheckGround())
+        {
+            player.InitializeJumpCount();
+            stateMachine.ChangeStateLogic(PlayerMovementEnums.IDLE);
+            return;
+        }
+
+        // 떨어지는 상태
+        if (player.rigid.velocity.y < 0)
+        {
+            stateMachine.ChangeStateLogic(PlayerMovementEnums.FALL);
+            return;
+        }
+
+        // 점프하는 상태
+        stateMachine.ChangeStateLogic(PlayerMovementEnums.JUMP);
     }
 
     public void FixedUpdate()
@@ -53,13 +69,18 @@ public class PlayerAttackState : IPlayerState
     public void OnEnter()
     {
         curIndex = 1;
-        player.animator.Play("Attack1");
-        curIndex++;
+        InitializeAttackAnimation();
     }
 
     public void OnExit()
     {
     }
 
+    void InitializeAttackAnimation()
+    {
+        player.animator.Play("Attack" + curIndex);
+        curIndex++;
+        if (curIndex > 3) curIndex = 1;
+    }
    
 }
