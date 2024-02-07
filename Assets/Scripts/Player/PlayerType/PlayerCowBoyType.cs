45995ef6fbb590272b85981952a086f4b02d3c31
mangruf <pinky0927@naver.com>
2024-02-08 00:16:14 +0900

    Feat : 카우보이 캐릭터 구현

---
+++ Assets/Scripts/Player/PlayerType/PlayerCowBoyType.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCowBoyType : PlayerType
+{
+    // ========== 특수 스킬 ==========
+    public override void SkillUpdate()
+    {
+
+    }
+    public override void SkillFixedUpdate()
+    {
+
+    }
+    public override void SkillOnEnter()
+    {
+
+    }
+    public override void SkillOnExit()
+    {
+
+    }
+}
